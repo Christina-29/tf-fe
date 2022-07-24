4c20977767648dcd99072ab51f591c09a1c1cf13
christina <connie0972001@gmail.com>
2022-07-24 23:56:28 +0930

    1

--- applications/aws_frontend/main.tf
+++ applications/aws_frontend/main.tf
@@ -5,43 +5,52 @@ terraform {
       version = "~>4.0"
     }
   }
+  backend "s3" {
+    bucket               = "bookinglet-terraform-state-file"
+    key                  = "bookinglet.tfstate"
+    region               = "ap-southeast-2"
+    encrypt              = true
+    workspace_key_prefix = "bookinglet-fe"
+  }
 }
 
 provider "aws" {
-  region                   = "ap-southeast-2"
+  region = "ap-southeast-2"
 }
 
 provider "aws" {
-  alias                    = "acm_provider"
-  region                   = "us-east-1"
-  # shared_credentials_file = "/users/chris/.aws/credentials"
+  alias  = "acm_provider"
+  region = "us-east-1"
 }
 
 module "s3" {
   source      = "../../modules/s3-route53-cdn/s3"
-  bucket_name = var.bucket_name
-  domain_name = var.domain_name
-  common_tags = var.common_tags
+  bucket_name = terraform.workspace == "prod" ? "shecan.link" : "${terraform.workspace}.shecan.link"
+  domain_name = terraform.workspace == "prod" ? "shecan.link" : "${terraform.workspace}.shecan.link"
+  common_tags = { Project = "bookinglet-${terraform.workspace}" }
+  environment = terraform.workspace
 }
 
 module "cdn" {
   source                    = "../../modules/s3-route53-cdn/cdn"
   www_regional_domain_name  = module.s3.www_bucket_regional_domain_name
   root_regional_domain_name = module.s3.root_bucket_regional_domain_name
-  bucket_name               = var.bucket_name
-  domain_name               = var.domain_name
+  bucket_name               = terraform.workspace == "prod" ? "shecan.link" : "${terraform.workspace}.shecan.link"
+  domain_name               = terraform.workspace == "prod" ? "shecan.link" : "${terraform.workspace}.shecan.link"
   origin_access_identity    = module.s3.origin_access_identity
   acm_certificate_arn       = module.route53.acm_certificate_arn
-  common_tags               = var.common_tags
+  common_tags               = { Project = "bookinglet-${terraform.workspace}" }
+  environment               = terraform.workspace
 }
 
 module "route53" {
   source                    = "../../modules/s3-route53-cdn/route53"
   zone_id                   = var.zone_id
-  common_tags = var.common_tags
-  domain_name               = var.domain_name
+  domain_name               = terraform.workspace == "prod" ? "shecan.link" : "${terraform.workspace}.shecan.link"
   root_bucket_alias_name    = module.cdn.root_bucket_alias_name
   root_bucket_alias_zone_id = module.cdn.root_bucket_alias_zone_id
   www_bucket_alias_name     = module.cdn.www_bucket_alias_name
   www_bucket_alias_zone_id  = module.cdn.www_bucket_alias_zone_id
+  ommon_tags                = { Project = "bookinglet-${terraform.workspace}" }
+  environment               = terraform.workspace
 }
--- applications/aws_frontend/terraform.tfvars
+++ applications/aws_frontend/terraform.tfvars
@@ -1,4 +1,2 @@
-bucket_name = "uat.shecan.link"
-domain_name = "uat.shecan.link"
-common_tags = { Project = "bookinglet-test" }
+
 zone_id = "Z0028220TJQ7DMP923PW"
